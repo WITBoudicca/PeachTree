07e5dc9a537358064d42e0f0ef840d730bc72d36
WITBoudicca <broddiccus@gmail.com>
2026-04-16 20:41:45 -0400

    menu sound track
    now has menu music, dynamically swaps to gameplay music on game start. does so instantly for reasons i cannot fathom which is an IMMENSE pissoff but there's only so much i can do

--- assets/entities/player/player_physics_script.gd
+++ assets/entities/player/player_physics_script.gd
@@ -1,17 +1,28 @@
 extends RigidBody3D
-
+#Physics/controll variables
 const SPEED = 15.0
 const MAXSPEED = 15.0 #for limiting our speed cause damn this peach can roll
-
-var camera
 var jumpDetect
 var grav = 1
 
+var camera
+#Audio Variables
+var gameMusic
+var menuMusic
+var maxVolume = 1.0
+var minVolume = 0.0
+var volumeTimer = 100.0
+var gameVolume = minVolume
+var menuVolume = maxVolume
 #signal jumpPressed
 var airborne = false
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	camera = $"../CameraSpringArm3D/Camera3D"
+	gameMusic = $"../CameraSpringArm3D/Camera3D/GameMusic"
+	menuMusic = $"../CameraSpringArm3D/Camera3D/MenuMusic"
+	gameMusic.set_volume_linear(gameVolume)
+	menuMusic.set_volume_linear(menuVolume)
 	jumpDetect = $"../JumpDetection"
 	Controller.game_Start.connect(_on_Game_Start)
 	Controller.game_Over.connect(_on_Game_End)
@@ -34,12 +45,17 @@ func _physics_process(delta: float) -> void:
 			linear_velocity.y += 7.5
 			airborne = true
 			#jumpPressed.emit()
-
+func _process(_delta: float) -> void: #i think my coding instructors would be right to kill me for this function but i love all my children and i'm too tired to think of something better
+		gameMusic.set_volume_linear(gameVolume)
+		menuMusic.set_volume_linear(menuVolume)
 #func _on_jump_delay_timeout(): #commenting this out and using collision to detect when the player is grounded to avoid double jumps and coyote jumps
 	#airborne = false			#still this timer code is good i'm gonna steal it for despawning old track bits
 
 func _on_Game_Start():
 	gravity_scale = grav
+	var tween = get_tree().create_tween()
+	tween.tween_property(self,"gameVolume",maxVolume,volumeTimer)
+	tween.tween_property(self,"menuVolume",minVolume,volumeTimer)
 	
 func _on_Game_End():
 	pass
